cca80a32032a1a6133d1110410c8f2db8be7d357
strbagus <satrio.baguswbw@gmail.com>
2022-09-23 07:47:40 +0700

    form input and message page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,64 +1,121 @@
 <script>
-import TheWelcome from '../components/TheWelcome.vue'
 import QRCode from 'qrcode-svg'
 export default {
-  components: {
-    TheWelcome,
-  },
   data() {
     return {
       form: ({
         title: null,
         msg: null,
+        background: '#0000ff',
       }),
     }
   },
+  mounted() {
+    document.getElementById("preview").style.background = this.form.background  
+  },
   methods: {
     saveBtn() {
-      let titleRaw = this.form.title
-      let msgRaw = this.form.msg
-      let title = encodeURI(titleRaw)
-      let msg = encodeURI(msgRaw)
-
-      let link = import.meta.env.VITE_BASE_URL+'message?title='+title+'&msg='+msg
-      console.log(link)
-      
       var qrcode = new QRCode({
-        content: link,
+        content: this.getLink(),
         container: "svg-viewbox",
         join: true 
       })
-      var svg = qrcode.svg();
-      document.getElementById("qrcode").innerHTML = svg;
+      var svg = qrcode.svg()
+      document.getElementById("qrcode").innerHTML = svg
+      document.getElementById("modal-qr").style.visibility = 'visible'
+      console.log(this.getLink())
+    },
+    getLink() {
+      let titleRaw = this.form.title
+      let msgRaw = this.form.msg
+      let bgRaw = this.form.background
+      let bg = bgRaw.replace('#', '')
+      let title = encodeURI(titleRaw)
+      let msg = encodeURI(msgRaw)
+      return import.meta.env.VITE_BASE_URL+'message?title='+title+'&msg='+msg+'&bg='+bg
+    },
+    backgroundColor(value){
+      document.getElementById("preview").style.background = value
+    },
+    redirectHome(){
+      document.getElementById("modal-qr").style.visibility = 'hidden'
     }
+  },
+  watch: {
+    'form.background': function (newVal) {
+      this.backgroundColor(newVal)
+    },
   },
 }
 </script>
 <template>
   <main>
-    <div class="container mx-auto">
-      <div class="bg-red-500">
-        <h1 class="text-3xl">{{ form.title }}</h1>
-        <p class="text-md">
-          {{ form.msg }}<br/>
-        </p>
+    <div>
+      <div class="form absolute px-5">
+        <h1 class="text-2xl text-yellow-500 mt-7">Preview</h1>
+        <div id="preview" class="m-1 px-5 pt-7 rounded">
+          <div class="message rounded shadow-md pt-3 px-2 bg-white">
+            <div v-if="form.title!=null">
+              <h1 class="text-2xl text-gray-700">{{ form.title }}</h1>
+              <hr>
+            </div>
+            <p class="text-md text-gray-700 pb-6 pt-4">
+              {{ form.msg }}<br/>
+            </p>
+          </div>
+          <p class="text-end py-2">{{ form.background }}</p>
+        </div>
+        <div class="box-input md:w-1/2 py-7">
+          <h1 class="text-red-500 text-2xl">Form Input</h1>
+          <div class="flex flex-col">
+            <label for="title">title</label>
+            <input type="text" class="form-input text-gray-700 rounded p-1" v-model="form.title" />
+          </div>
+          <div class="flex flex-col">
+            <label for="msg">Message</label> 
+            <textarea type="text" class="form-input text-gray-700 rounded" v-model="form.msg" required/>
+          </div>
+          <div class="flex flex-col">
+            <label for="title">Background Color</label>
+            <input type="color" class="form-input text-gray-700 rounded p-1" v-model="form.background" />
+          </div>
+          <div class="flex justify-end my-3 mx-5">
+            <button class="bg-blue-600 rounded px-3 py-1 text-white" @click="saveBtn" :disabled="form.msg==null">Simpan</button>
+          </div>
+        </div>
       </div>
-      <div class="box-input md:w-1/2 px-5 py-7">
-        <h1 class="text-red-500 text-2xl">Form Input</h1>
-        <div class="flex flex-col">
-          <label for="title">title</label>
-          <input type="text" class="form-input mx-5 text-gray-700 rounded p-1" v-model="form.title" />
-        </div>
-        <div class="flex flex-col">
-          <label for="msg">Message</label> 
-          <textarea type="text" class="form-input mx-5 text-gray-700 rounded" v-model="form.msg" />
-        </div>
-        <div class="flex justify-end my-3 mx-5">
-          <button class="bg-blue-600 rounded px-3 py-1 text-white" @click="saveBtn" :disable="form.msg==null">Simpan</button>
+      <div id="modal-qr" class="absolute p-5">
+        <div class="qr-box bg-red-500">
+          <div id="qrcode" class=""></div>
+          <p class="text-center my-5 italic">
+            maaf programmer nya cupu gabisa convert ext .svg ke .jpg/.png jadi screenshot dulu ya
+          </p>
+          <div class="flex justify-center">
+            <button class="bg-blue-600 rounded px-3 py-1 text-white" @click="redirectHome()">Selesai</button>
+          </div>
         </div>
       </div>
-      <TheWelcome />
-      <div id="qrcode"></div>
     </div>
   </main>
 </template>
+<style>
+  #modal-qr{
+    height: 100vh;
+    width: 100%;
+    background: rgba(14, 14, 14, 0.9);
+    visibility: hidden;
+    
+  }
+  .form{
+    width: 100%;
+    min-height: 100vh;
+  }
+  .qr-box{
+    position: relative;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    max-width: 300px;
+    max-height: 300px;
+  }
+</style>
--- src/views/MessageView.vue
+++ src/views/MessageView.vue
@@ -11,15 +11,31 @@ export default {
     const urlParams = new URLSearchParams(queryString);
     let title = urlParams.get('title')
     let message = urlParams.get('msg')
+    let bg = urlParams.get('bg')
     this.title = title
     this.msg = message
-    console.log(title + message)
+    document.body.style.background = '#'+bg
+    console.log(title + message + bg)
   },
 }
 </script>
 <template>
-  <div class="about">
-    <h1 class="text-3xl text-yellow-500">{{ title }}</h1>
-    <h1 class="text-xl text-red-500">{{ msg }}</h1>
+  <div class="about px-2">
+    <div class="messages bg-white rounded shadow-lg md:w-1/3 w-full pad py-7 px-5">
+      <h1 class="text-3xl text-gray-800">{{ title }}</h1>
+      <hr>
+      <p class="text-xl text-gray-700 pt-5">{{ msg }}</p>
+    </div>
   </div>
 </template>
+<style>
+  .about{
+    min-height: 100vh;
+  }
+  .messages{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+</style>
