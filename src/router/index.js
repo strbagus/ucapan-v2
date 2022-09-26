46a13fbf618b4749b546726fdcb422986c466bee
strbagus <satrio.baguswbw@gmail.com>
2022-09-26 21:52:45 +0700

    Debugging router issue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import MessageView from '../views/MessageView.vue'
+import TestView from '../views/TestView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -11,18 +11,18 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/test',
+      name: 'message',
+      component: TestView
+    },
+    {
       path: '/message',
       name: 'message',
-      component: MessageView
-    },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/MessageView.vue')
+    }
   ]
 })
 
---
+++ src/views/TestView.vue
@@ -0,0 +1,31 @@
+<script>
+    export default {
+      data() {
+        return {
+          title: 'Lorem Ipsum Title',
+          msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Risus viverra adipiscing at in tellus integer feugiat scelerisque varius.',
+        }
+      },
+    }
+    </script>
+    <template>
+      <div class="about px-2">
+        <div class="messages bg-white rounded shadow-lg md:w-1/3 pad py-7 px-5">
+          <h1 class="text-2xl text-gray-800">{{ title }}</h1>
+          <hr>
+          <p class="text-md text-gray-700 pt-5">{{ msg }}</p>
+        </div>
+      </div>
+    </template>
+    <style>
+      .about{
+        min-height: 100vh;
+      }
+      .messages{
+        position: absolute;
+        width: 95%;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+      }
+    </style>
