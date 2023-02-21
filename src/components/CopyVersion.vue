54a85ee1c6949ff491470b69af22fbc5ec05d8f2
strbagus <satrio.baguswbw@gmail.com>
2023-02-22 03:39:46 +0700

    update copyright style

--- src/components/CopyVersion.vue
+++ src/components/CopyVersion.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="mx-2 bottom-1 absolute right-0">
+  <footer class="mx-2 bottom-1 absolute right-10 opacity-30">
     <span class="text-gray-100 text-xxs md:text-xs" style="text-shadow: 1px 1px 5px black">
       by <a href="https://github.com/strbagus" title="Github strbagus" class="text-gray-200 font-medium hover:text-white">strbagus</a>
     </span>
