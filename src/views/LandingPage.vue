13086579f102975f2c2b1bdbaff6ddcffb55cc0e
strbagus <satrio.baguswbw@gmail.com>
2023-02-26 00:31:46 +0700

    update landing page

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="h-screen w-full md:w-1/2">
+        <div class="center w-3/4 md:w-max-3xl px-3">
+            <div class="text-sm text-white text-justify">
+                Say Hi to your whoever that is with this letter in envelope.
+                <br>
+                You just Write, Screenshot QR, Send. That person will Scan QR, Click the envelope and the letter will pop.
+            </div>
+            <a href="/write">
+                <div class="bg-cyan-500 hover:bg-cyan-600 text-white duration-400 px-5 relative py-1 rounded-lg my-3 inline-block float-right md:float-left">Write Now!</div>
+            </a>
+        </div>
+    </div>
+</template>
+<style>
+.center {
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -45%);
+}
+</style>
